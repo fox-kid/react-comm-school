a83f2c5a79719dd0f08b6a1ac5d1698b1da303a7
Ani Lezhava <alezh15@freeuni.edu.ge>
2021-05-03 06:03:02 -0700

    https://github.com/fox-kid/react-comm-school.git

--- lesson1/ex1.js
+++ lesson1/ex1.js
@@ -1,7 +1,7 @@
 function equals(x, y) {
     if(x !== x && y !== y){   // Checks if both values are Number NaN
         return true;
-    }else if(x === 0 && y === 0 && 1 / x !== 1 / y ){ // Makes sure that 0 and -0 are not equal
+    }else if(x === 0 && y === 0 && 1 / x !== 1 / y ){ // Makes sure that 0 and -0 are notgi equal
         return false;
     }else if(x === y){
         return true;
@@ -21,13 +21,13 @@ console.log(equals("john", "doe") === false);
 console.log(equals(false, true) === false);
 console.log(equals(undefined, null) === false);
 
-console.log(equals("john", "john") === true); ``
+console.log(equals("john", "john") === true); 
 console.log(equals(-0, -0) === true);
 
 console.log(equals(5, 5) === true);
 console.log(equals(false, false) === true);
 console.log(equals(null, null) === true);
-console.log(equals(undefined, undefined) === true);  ``
+console.log(equals(undefined, undefined) === true);  
 console.log(equals(NaN, NaN) === true);
 
 console.log(equals(0, 0) === true); 
