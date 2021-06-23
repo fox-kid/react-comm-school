befda0563a3304e217c3eb7c7df9dedee28eb35d
fox-kid <alezh15@freeuni.edu.ge>
2021-06-23 11:03:47 -0700

    still no load more and favorites

--- finalproject/src/pages/Search/index.js
+++ finalproject/src/pages/Search/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navigation from "../../components/Navigation";
-// import styles from "./Search.module.css";
+import styles from "./Search.module.css";
 import SearchBar from "../../components/SearchBar";
 import UserCard from "../../components/UserCard";
 
@@ -11,14 +11,17 @@ function Search() {
     <main>
       <Navigation />
       <SearchBar setFiltered={setFiltered} />
-      {filtered &&
+      {filtered.length ? (
         filtered.map((user) => (
           <UserCard
             key={user.login}
             username={user.login}
             img={user.avatar_url}
           />
-        ))}
+        ))
+      ) : (
+        <p className={styles.no_users_text}>Type correct username</p>
+      )}
     </main>
   );
 }
